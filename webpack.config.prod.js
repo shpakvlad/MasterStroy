8fbb052f919b2fdb4886f1a4b8183420706671c2
shpakvlad <vladyslav.shpakov@gmail.com>
2025-01-08 21:11:22 +0200

    add gh-pages to script

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,9 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
-  output: {
-    publicPath: '/MasterStroy/', // Укажите путь к вашему репозиторию
-  },
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
@@ -27,14 +24,5 @@ module.exports = merge(common, {
         { from: 'site.webmanifest', to: 'site.webmanifest' },
       ],
     }),
-  ],
-  rules: [
-    {
-      test: /\.(png|jpe?g|gif|svg|webp)$/i,
-      type: 'asset/resource',
-      generator: {
-        filename: 'img/[name][ext]',
-      },
-    },
   ],
 });
